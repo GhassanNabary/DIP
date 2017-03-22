813912ce39077a61fc3dd13e8a9044fd4c43f897
GhassanNabary <ghassanv8@gmail.com>
2017-03-22 13:19:41 +0200

    ComputeCubicInterpolationCoefficients added

--- ass1.py
+++ ass1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import cv2
 
 
@@ -27,17 +28,60 @@ class RemoveElementsFromImage:
         return smallerImage
 
 
-    def  ComputeCubicInterpolationCoefficients(self):
+    def  ComputeCubicInterpolationCoefficients(self,top,left,i,j,img):
         print 'here'
+        right_distances=[-1.25,-0.25,0.75,1.75]
+        left_distances = [-1.75, -0.75, 0.25, 1.25]
+        top_distances=[-1.25, -0.25, 0.75, 1.75]
+        bottom_distances=[-1.75, -0.75, 0.25, 1.25]
+
+        #compute Cx
+        if(left):
+            x=left_distances
+        else:
+            x=right_distances
+        if(top):
+            y=top_distances
+        else:
+            y=bottom_distances
+
+
+
+#TODO image bounderies
+        bx=[img[ math.ceil(i+x[0]),j],img[math.ceil(i+x[1]),j],img[math.ceil(i+x[2]),j],img[math.ceil(i+x[3]),j]]
+        by=[img[ i,math.ceil(j+y[0])],i,img[math.ceil(j+y[1])],i,img[math.ceil(j+y[2])],img[i,math.ceil(j+y[3])]]
+
+        cx=np.polyfit(x,bx,3)
+        cy=np.polyfit(y,by,3)
+
+        m = np.outer(cx, cy)
+
+        s = m.sum()
+
+        return m/float(s)
+
+    def  RemoveElements(self,Image,colors):
+
 
 
 if __name__ == '__main__':
 
-    img=cv2.imread('prettyGirl.jpg',0)
-    cv2.imshow('image',img)
-    cv2.waitKey(0)
-    removeElm=RemoveElementsFromImage()
-    smaller=removeElm.BuildImagePyramid(img,3)
-    print 'finished'
-    cv2.imshow('smaller',smaller)
-    cv2.waitKey(0)
+    # img=cv2.imread('prettyGirl.jpg',0)
+    # cv2.imshow('image',img)
+    # cv2.waitKey(0)
+    # removeElm=RemoveElementsFromImage()
+    # smaller=removeElm.BuildImagePyramid(img,1)
+    # print 'finished'
+    # cv2.imshow('smaller',smaller)
+    # cv2.waitKey(0)
+    cx=[1,1,1,1]
+    cy=[1,1,1,1]
+    # vec = [(i * j) for (i, j) in zip(cx, cy)]
+    a = np.outer(cx, cy)
+    # b = np.matrix(a)
+    s = a.sum()
+    print 1
+    print type(a)
+    b = a / float(s)
+    print b
+
